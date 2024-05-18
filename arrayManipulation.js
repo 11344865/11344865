f6e300150360e85f84c3ab79a118d28121ba6dda
11344865 <fknwinfang@st.ug.edu.gh>
2024-05-18 18:34:49 +0000

    Update arrayManipulation.js

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,1 +1,16 @@
+arrayManipulation.js
+processArray:
+Here is the JavaScript file arrayManipulation.js with the function
+    if (num % 2 === 0) {
+      return num ** 2;
+    } else {
+      return num * 3;
+    }
+  });
+}
 
+// Example usage:
+const originalArray = [1, 2, 3, 4, 5, 6];
+const processedArray = processArray(originalArray);
+console.log(processedArray); // Output: [3, 4, 9, 16, 15, 36]
+```
